a66fe1571306412f301d1f69668d1116f836b433
raguel <raguel_he@foxmail.com>
2018-11-30 00:33:57 +0800

    add config

---
+++ config/config.py
@@ -0,0 +1,37 @@
+
+
+REPOSITORY_JAVA_DB = {
+    'host':'',
+    'db':'',
+    'user':'',
+    'passwd':'',
+    'charset':'utf8mb4'
+}
+
+REPOSITORY_TEST_DB = {
+    'host':'localhost',
+    'db':'',
+    'user':'',
+    'passwd':'',
+    'charset':'utf8mb4'
+}
+
+GITHUB = {
+    'account':'github账号',
+    'password':'github密码',
+    'token':''
+}
+
+REDIS = {
+    'host':'',
+    'db':'',
+    'password':''
+}
+
+MAX_THREAD = 3
+
+# 默认测试路径
+DEFAULT_PATH_TEST = ''
+
+# 默认下载路径
+DEFAULT_PATH = ''
